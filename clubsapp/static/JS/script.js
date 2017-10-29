71596f273a5ed459c3ac365adee8caa1718f5a0c
s2g2r.2g2rw2l <sagban@github.com>
2017-10-29 16:37:44 +0530

    graph api for event added/event front end modified

--- clubsapp/static/JS/script.js
+++ clubsapp/static/JS/script.js
@@ -659,7 +659,39 @@ $(function() {
 });
 
 
+$(function(){ /* to make sure the script runs after page load */
 
+	$('.event-desp').each(function(event) { /* select all divs with the item class */
+
+        var max_length = 150;
+        /* set the max content length before a read more link will be added */
+
+        if ($(this).html().length > max_length) { /* check for content length */
+
+            var short_content = $(this).html().substr(0, max_length);
+            /* split the content in two parts */
+            var long_content = $(this).html().substr(max_length);
+
+            $(this).html(short_content +
+                '<a href="#" class="read_more">..Read More</a>' +
+                '<span class="more_text" style="display:none;">' + long_content + '</span>');
+            /* Alter the html to allow the read more functionality */
+
+            $(this).find('a.read_more').click(function (event) { /* find the a.read_more element within the new html and bind the following code to it */
+
+                event.preventDefault();
+                /* prevent the a from changing the url */
+                $(this).hide();
+                /* hide the read more button */
+                $(this).parents('.event-desp').find('.more_text').show();
+                /* show the .more_text span */
+
+            });
+
+        }
+
+    });
+});
 
 
 
--- clubsapp/views.py
+++ clubsapp/views.py
@@ -4,6 +4,8 @@ from django.core.context_processors import csrf
 
 from django.template import RequestContext
 from portalapp.models import *
+import requests
+import json
 #
 # Create your views here.
 #  For trial use this
@@ -22,6 +24,21 @@ def clubs( request ):
 
     return render_to_response ( 'VnitClubsHome.html' , args )
 
+def fb_catch(url):
+
+    #fb token
+    token = 'EAACEdEose0cBAOERMW3K7qov7F8Y0abyXwFYd1BJFIS8bHi6FYuYucysLWW4GhKjp5ZBvgp6yTVfP8ByG4uKgCDZAVODATPZBQEv1x8zkiC8tj0a5CCZAuyddz6BL8TVAG00urAAxYDgx45ZBrFDBtVHBRmdsVaQZBZBHeJl9MNBZBZCI2sRYHHeGP3nUhwqCdWgF200yf7mmrb0Xm1VPRY64'#updating url
+    url = url + token
+    #making req to fb
+    jsondata = requests.get(url)
+    #convert to python data
+    pydata = jsondata.json()
+    print pydata
+    return pydata
+
+
+
+
 def clubHome(request,clubName):
      # Take pagename, find in db ,extract data and use it here.
 
@@ -32,5 +49,22 @@ def clubHome(request,clubName):
     args.update ( csrf ( request ) )
     args.update ( headerdb ( request ) )
 
-    print clubName
+    clubfbid = '125409010890443'
+
+    eventurl = 'https://graph.facebook.com/v2.10/'+clubfbid+'/events?access_token='
+    data = fb_catch(eventurl)
+    args.update({'data': data['data']})
     return render_to_response ( 'ClubHome.html' , args )
+
+'''
+access_token='EAACEdEose0cBAC4Jdyi8isEwZCm7hxWJ9rfAXFbJbGRBC2EnL62E0FZBxcQqN7WqAOZBvG9D5Vp3E3ZBGPQB6cLc0lazpa71ZCaZBch9zFnbGSl2rl1HffE0mNVjpLIKaeHS1OL6iEcjxTgPeYn2g6PqQP0Y6spAsZB8SoxZCkZCcAZA3YmZAdqHO0cGR02wiLmWlbCAR1l4WQV95kHRP6uB5VH'
+app_id = "1552794168112376"                       # Obtained from https://developers.facebook.com/
+client_secret = "dc00b43c9493341844ee87f137821b02"         # Obtained from https://developers.facebook.com/
+
+link = "https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=" + app_id +"&client_secret=" + client_secret + "&fb_exchange_token=" + access_token
+s = requests.Session()
+token = s.get(link).content
+token = token.split("&")[0]                 # this strips out the expire info (now set set about 5184000 seconds, or 60 days)
+token = token.strip("access_token=")        # Strips out access token
+print token
+'''
