3ebb1e32124f5284a23734040da01a6b3c1b3ee9
PriyankJPI07-sopho <priyankjpi07@gmail.com>
2017-10-30 22:08:31 +0530

    load_data.py edit

--- load_data.py
+++ load_data.py
@@ -41,16 +41,114 @@ for row in dataReader:
         club.contact = contact_detail
         club.save()
 
+        clubmember = ClubMember()
+        clubmember.id = row[0]
+        clubmember.basicDetails = row[1]
+        memberPost = Post.object.get(id=row[2])
+        print memberPost
+        clubmember.post = memberPost
+        clubmember.photograph = row[3]
+        clubmember.dateOfJoin = row[4]
+        clubmember.dateOfLeave = row[5]
+        clubmember.save()
 
-        '''
-            contact = ContactDetails()
-            contact.id = row[0]
-            contact.email = row[1]
-            contact.website = row[2]
-            contact.telephone1 = row[3]
-            contact.telephone2 = row[4]
-            contact.save()
-        '''
+        contact = ContactDetails()
+        contact.id = row[0]
+        contact.email = row[1]
+        contact.website = row[2]
+        contact.telephone1 = row[3]
+        contact.telephone2 = row[4]
+        contact.save()
+
+        post = Post()
+        post.id = row[0]
+        post.shortName = row[1]
+        post.longName = row[2]
+        post.displayName = row[3]
+        post.save()
+
+        photos = Photos()
+        photos.id = row[0]
+        photos.photographs = row[1]
+        photos.details = row[2]
+        photos.dateOfCapture = row[3]
+        photos.save()
+
+        event = Event()
+        event.id = row[0]
+        event.shortName = row[1]
+        event.longName = row[2]
+        event.displayName = row[3]
+        event.place = row[4]
+        event.time = row[5]
+        event.date = row[6]
+        event.save()
+
+        activity = Activity()
+        activity.id = row[0]
+        activity.title = row[1]
+        activity.description = row[2]
+        activity.date = row[3]
+        activity.save()
+
+        cer = ClubEventRelationship()
+        cer.id = row[0]
+        cerclub = Club.object.get(id=row[1])
+        print cerclub
+        cer.club = cerclub
+        cerevent = Event.object.get(id=row[2])
+        print cerevent
+        cer.event = cerevent
+        cer.save()
+
+        car = ClubActivityRelationship()
+        car.id = row[0]
+        carclub = Club.object.get(id=row[1])
+        print carclub
+        car.club = carclub
+        caractivity = Activity.object.get(id=row[2])
+        print caractivity
+        car.activity = caractivity
+        car.save()
+
+        cpr = ClubPhotoRelationship()
+        cpr.id = row[0]
+        cprclub = Club.object.get(id=row[1])
+        print cprclub
+        cpr.club = cprclub
+        cprphoto = Photos.object.get(id=row[2])
+        print cprphoto
+        cpr.photo = cprphoto
+        cpr.save()
+
+        epr = EventPhotoRelationship()
+        epr.id = row[0]
+        eprevent = Event.object.get(id=row[1])
+        print eprevent
+        epr.event = eprevent
+        eprphoto = Photos.object.get(id=row[2])
+        print eprphoto
+        epr.photo = eprphoto
+        epr.save()
+
+        apr = ActivityPhotoRelationship()
+        apr.id = row[0]
+        apractivity = Activity.object.get(id=row[1])
+        print apractivity
+        apr.activity = apractivity
+        aprphoto = Photos.object.get(id=row[2])
+        print aprphoto
+        apr.photo = aprphoto
+        apr.save()
+
+
+
+
+
+
+
+
+
 
 
 
